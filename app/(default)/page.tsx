ab6596e71fdadfa4317d39d8a9f3f16e12ebe4e0
Karan Jagtap <karan@Karans-MacBook-Air.local>
2024-02-12 18:49:17 +0530

    fix: reverted description metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,8 +8,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "W.S. Constructions",
-  description:
-    "Established in 1991, W.S. Constructions has been recognized for unwavering quality and reliability in the construction industry. The inception of our journey unfolded when Mr. Hemant Kasbekar (B. E. Civil) and Mr. Deelip Kasbekar (D.C.E.) united their expertise, giving rise to the visionary concept of a joint business venture.",
+  description: "Engineers and Contractors",
 };
 
 export default function Home() {
